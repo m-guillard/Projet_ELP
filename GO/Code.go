650cddeda05039a00f32087d504795601b7eed59
AliceWck <alice.invernizzi@outlook.fr>
2025-01-13 17:47:41 +0100

    Màj main code, création déroulé, rassemblement codes

--- GO/Code.go
+++ GO/Code.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
@@ -9,63 +10,22 @@ import (
 	"time"
 )
 
-// Structure partagée
-type SafeMap struct {
-	mu      sync.Mutex
-	map_lev map[string]map[string]int
-}
-
-// Initialise la matrice pour l'algorithme de Leveinstein
-func matrice(motA string, motB string) [][]int {
-
-	var longMotA, longMotB int = len(motA) + 1, len(motB) + 1
-
-	mat := make([][]int, longMotA) // Le nombre de ligne correspond à la longueur du mot A+1
-	for i := range mat {           // Le nombre de colonne correspond à la longueur du mot B+1
-		mat[i] = make([]int, longMotB)
-	}
-
-	// On remplit la matrice qui est à 0
-	for i := 0; i < longMotA; i++ {
-		// Première colonne rempli de 0 à la longueur du mot A
-		mat[i][0] = i
-	}
-	for i := 0; i < longMotB; i++ {
-		// Première ligne rempli de 0 à la longueur du mot B
-		mat[0][i] = i
-	}
-
-	return mat
-}
-
-// Renvoie un map mis à jour. Le map a pour clé le nom de la première base de données
-// Sa valeur est une map qui a pour clé le nom de la seconde base de données et comme valeur la distance de Levenshtein
-func (s *SafeMap) MapLevenshtein(motA string, motB string, dist int) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	valeur, existe := s.map_lev[motA] // Récupère le map associé à la clé A
-	// A faire : GESTION DE DOUBLONS
-	if existe == false {
-		valeur = make(map[string]int)
-	}
-	valeur[motB] = dist // On ajoute dans le map, le nouveau mot avec sa distance de Levenshtein
-	s.map_lev[motA] = valeur
-}
-
-func (s *SafeMap) Display() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	fmt.Printf("\nDictionnaire:\n")
-	for k, v := range s.map_lev {
-		fmt.Printf("%v \n", k)
-		for k2, v2 := range v {
-			fmt.Printf("-> %v = %v \n", k2, v2)
+// Fonction pour obtenir la valeur minimale parmi 3 entiers
+func min(a, b, c int) int {
+	if a < b {
+		if a < c {
+			return a
 		}
+		return c
 	}
+	if b < c {
+		return b
+	}
+	return c
 }
 
 func dico_to_csv(map_lev map[string]map[string]int, date string) {
-	// Construction nom fichier avec la datee
+	// Construction nom fichier avec la date
 	fileName := fmt.Sprintf("output_%s.csv", date)
 
 	// Création fichier CSV
@@ -102,21 +62,114 @@ func dico_to_csv(map_lev map[string]map[string]int, date string) {
 
 }
 
-// Fonction pour obtenir la valeur minimale parmi 3 entiers
-func min(a, b, c int) int {
-	if a < b {
-		if a < c {
-			return a
-		}
-		return c
-	}
-	if b < c {
-		return b
-	}
-	return c
+// Structure partagée
+type SafeMap struct {
+	mu      sync.Mutex
+	map_lev map[string]map[string]int
 }
 
-func comparaison(mot_A, mot_B string, matrice_vide [][]int) {
+func extractionColonne(nomFichier string, nomColonne string) string {
+	// Lecture de nomFichier
+	fichierOriginal, err := os.Open(nomFichier)
+	if err != nil {
+		fmt.Printf("Erreur lors de l'ouverture du fichier : %v\n", err)
+		return ""
+	}
+	defer fichierOriginal.Close()
+
+	// Création du fichier
+	nomNvFichier := strings.Split(nomFichier, ".")[0] + "_" + nomColonne + ".csv"
+	nvFichier, err := os.OpenFile(nomNvFichier, os.O_CREATE|os.O_WRONLY|os.O_APPEND|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Printf("Erreur lors de l'ouverture du fichier : %v\n", err)
+		return ""
+	}
+	defer nvFichier.Close()
+
+	scanner := bufio.NewScanner(fichierOriginal)
+
+	// Parcourt du fichier ligne par ligne pour extraire la colonne
+	var indiceColonne int = -1
+	for scanner.Scan() {
+		ligne := strings.Split(scanner.Text(), ";")
+		if indiceColonne == -1 { //Première ligne
+			// On trouve la place de la colonne
+			for index, elt := range ligne {
+				if elt == nomColonne {
+					indiceColonne = index
+				}
+			}
+			if indiceColonne == -1 {
+				fmt.Printf("Nom de colonne non trouvée dans le CSV\n")
+				return ""
+			}
+		}
+		_, err := nvFichier.WriteString(ligne[indiceColonne] + "\n")
+		if err != nil {
+			fmt.Printf("Erreur lors de l'écriture de la ligne : %v\n", err)
+			return ""
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Erreur de lecture du fichier : %v\n", err)
+	}
+
+	return nomNvFichier
+}
+
+// Renvoie un map mis à jour. Le map a pour clé le nom de la première base de données
+// Sa valeur est une map qui a pour clé le nom de la seconde base de données et comme valeur la distance de Levenshtein
+func (s *SafeMap) MapLevenshtein(motA string, motB string, dist int, dist_max int) {
+	if dist <= dist_max {
+		// Ne pas prendre en compte les données si distance de Levenshtein trop élevée
+		s.mu.Lock()
+		defer s.mu.Unlock()
+		valeur, existe := s.map_lev[motA] // Récupère le map associé à la clé A
+		// A faire : GESTION DE DOUBLONS
+		if existe == false {
+			valeur = make(map[string]int)
+		}
+		valeur[motB] = dist // On ajoute dans le map, le nouveau mot avec sa distance de Levenshtein
+		s.map_lev[motA] = valeur
+	}
+}
+
+func (s *SafeMap) Display() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	fmt.Printf("\nDictionnaire:\n")
+	for k, v := range s.map_lev {
+		fmt.Printf("%v \n", k)
+		for k2, v2 := range v {
+			fmt.Printf("-> %v = %v \n", k2, v2)
+		}
+	}
+}
+
+// Initialise la matrice pour l'algorithme de Leveinstein
+func matrice(motA string, motB string) [][]int {
+
+	var longMotA, longMotB int = len(motA) + 1, len(motB) + 1
+
+	mat := make([][]int, longMotA) // Le nombre de ligne correspond à la longueur du mot A+1
+	for i := range mat {           // Le nombre de colonne correspond à la longueur du mot B+1
+		mat[i] = make([]int, longMotB)
+	}
+
+	// On remplit la matrice qui est à 0
+	for i := 0; i < longMotA; i++ {
+		// Première colonne rempli de 0 à la longueur du mot A
+		mat[i][0] = i
+	}
+	for i := 0; i < longMotB; i++ {
+		// Première ligne rempli de 0 à la longueur du mot B
+		mat[0][i] = i
+	}
+
+	return mat
+}
+
+func matrice_lev(mot_A, mot_B string, matrice [][]int) int {
 	liste_A := strings.Split(mot_A, "")
 	fmt.Println(liste_A)
 	liste_B := strings.Split(mot_B, "")
@@ -130,86 +183,71 @@ func comparaison(mot_A, mot_B string, matrice_vide [][]int) {
 				cout_substitution = 0
 			}
 			// Calcul des coûts : insertion, suppression, substitution
-			insertion := matrice_vide[i][j-1] + 1
-			suppression := matrice_vide[i-1][j] + 1
-			substitution := matrice_vide[i-1][j-1] + cout_substitution
-			matrice_vide[i][j] = min(insertion, suppression, substitution)
+			insertion := matrice[i][j-1] + 1
+			suppression := matrice[i-1][j] + 1
+			substitution := matrice[i-1][j-1] + cout_substitution
+			matrice[i][j] = min(insertion, suppression, substitution)
 
 		}
 	}
 
 	// Affichage de la matrice remplie
-	fmt.Println("Matrice remplie :")
-	for _, row := range matrice_vide {
-		for _, value := range row {
-			fmt.Printf("%d ", value)
-		}
-		fmt.Println()
-	}
+	// fmt.Println("Matrice remplie :")
+	// for _, row := range matrice_vide {
+	// 	for _, value := range row {
+	// 		fmt.Printf("%d ", value)
+	// 	}
+	// 	fmt.Println()
+	// }
+	return matrice[len(liste_A)][len(liste_B)]
+}
+
+// matrice_lev : remplit la matrice avec l'algo
+// extraction_colonne : prend fichier csv et renvoie un fichier csv avec une seule colonne
+// matrice : initialise la matrice vide avant l'algo
+// MapLevenshtein : renvoie un dictionnaire où la clé est un mot, et les valeurs sont des dictionnaires d'un mot et de sa distance au premier mot
+// Display : affiche les dico (pour débuggage)
+// min : trouve le min entre 3 nombres
+// dico_to_csv : transforme le dico final en csv final
+// safemap : création du type structure partagée
+
+func deroule() {
+	// On a deux mots
+	var mot_A string = "CHAT"
+	var mot_B string = "CHIEN"
+	dist_max := 1
+
+	// On crée une matrice vide de longueur adaptée à ces deux mots
+	matrice_vide := matrice(mot_A, mot_B)
+
+	// On remplit cette matrice avec l'algo de Levenshtein et on retourne la valeur de la distance de Levenshtein
+	Distance_Levenshtein := matrice_lev(mot_A, mot_B, matrice_vide)
+
+	// On ajoute ce lien entre les mots au dictionnaire partagé (si la distance est inférieure à dist_max)
+	MapLevenshtein(mot_A, mot_B, Distance_Levenshtein, dist_max)
+
 }
 
 func main() {
-	mot_A := "ALICE"
-	mot_B := "MARION"
-
-	// Matrice vide donnée par un autre code
-	matrice_vide := [][]int{
-		{0, 1, 2, 3, 4, 5, 6},
-		{1, 0, 0, 0, 0, 0, 0},
-		{2, 0, 0, 0, 0, 0, 0},
-		{3, 0, 0, 0, 0, 0, 0},
-		{4, 0, 0, 0, 0, 0, 0},
-		{5, 0, 0, 0, 0, 0, 0},
-	}
-
-	map_lev := map[string]map[string]int{
-		"nom": {
-			"nom_2": 2,
-			"nom_3": 3,
-		},
-		"nom_autre": {
-			"nom_2": 20,
-			"nom_4": 40,
-		},
-	}
-
-	now := time.Now()
-	anneeMoisJour := now.Format("2006_01_02")
-
-	comparaison(mot_A, mot_B, matrice_vide)
-	dico_to_csv(map_lev, anneeMoisJour)
-}
-
-func main() {
-	var motA string = "CHAT"
-	var motB string = "CHIEN"
-	var mat [][]int = matrice(motA, motB)
-	for i := 0; i < len(mat); i++ {
-		for j := 0; j < len(mat[i]); j++ {
-			fmt.Printf("%v", mat[i][j])
-		}
-		fmt.Printf("\n")
-	}
-	var wg sync.WaitGroup
-
-	// Partie avec la goroutine
+	// créer dico partagé, accès
+	var wg sync.WaitGroup                                  // crée groupe d'attente, on y ajoute les go routines, pour attendre qu'elles finissent pour créer le csv
 	c := SafeMap{map_lev: make(map[string]map[string]int)} // Création du channel
-	sous_ex2 := make(map[string]int)
-	sous_ex2["chat"] = 1
-	c.map_lev["chien"] = sous_ex2
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		c.MapLevenshtein("chien", "niche", 5)
-	}()
+
+	// après, avec go routines, on aura pleins de mots à comparer
+	// for qui lance les go routines, de la syntaxe suivante
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		c.MapLevenshtein("dinosaure", "brebis", 12)
 	}()
-	wg.Wait()
-	c.Display()
+
+	// récupération des go routines, transfo
+	wg.Wait() // attend que toutes les goroutines soient terminées
+
+	// param nomenclature fichier csv final
+	now := time.Now()
+	anneeMoisJour := now.Format("2006_01_02")
+
+	dico_to_csv(c.map_lev, anneeMoisJour)
 
 }
-
-// à faire : main avec tout le code, serveur tcp
