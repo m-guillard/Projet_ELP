eb9180819344a7d0aedece30ac015897c154885c
Marion G. <ma86gui@gmail.com>
2025-01-17 00:50:15 +0100

    Explication du code et transfert des paramètres du client vers le serveur

--- GO/tcp_client.go
+++ GO/tcp_client.go
@@ -8,6 +8,7 @@ import (
 )
 
 func gestion_erreur(err error, message string) {
+	// Arrête le programme si il y a une erreur et affiche un message d'erreur dans le terminal
 	if err != nil {
 		fmt.Printf("Erreur : %s - %v\n", message, err)
 		os.Exit(1) // Arrête le programme avec un code de retour 1 (indiquant une erreur)
@@ -31,28 +32,32 @@ func lecture_csv(chemin string) []byte {
 	gestion_erreur(err, "Lecture du fichier")
 	defer file.Close()
 
-	// Retourne l'intégralité du fichier en binaire et le nombre d'octets
+	// Retourne l'intégralité du fichier en binaire
 	return data
 }
 
 func ecriture_csv(data []byte, fichier string) {
+	// Crée un nouveau fichier, si il existe, il est réinitialisé
 	nvFichier, err := os.OpenFile(fichier, os.O_CREATE|os.O_WRONLY|os.O_APPEND|os.O_TRUNC, 0644)
-	defer nvFichier.Close()
+	defer nvFichier.Close() // Ferme le fichier a la fin de la fonction
 	gestion_erreur(err, "Creation du fichier")
+
+	// Ecris les données dans le fichier créé
 	err = os.WriteFile(fichier, data, 0644)
 	gestion_erreur(err, "Ecriture dans fichier")
 }
 
 func main() {
-	gob.Register([]byte{})
+	gob.Register([]byte{}) // Enregistre le type []byte pour le gob
+
 	// On récupère les arguments de la ligne de commande
 	arguments := os.Args
-	if len(arguments) != 5 {
+	if len(arguments) != 7 { // On sort du main si les arguments ne sont pas corrects
 		fmt.Printf("Argument incorrect \n")
-		fmt.Printf("go run programme.go <adresse serveur> <port> <chemin base de donnee 1> <chemin base de donnee 2>")
+		fmt.Printf("go run programme.go <adresse serveur> <port> <chemin base de donnee 1> <chemin base de donnee 2> <nombre goroutines> <distance Levenshtein limite>")
 		return
 	}
-	serv, port, bdd1, bdd2 := arguments[1], arguments[2], arguments[3], arguments[4]
+	serv, port, bdd1, bdd2, nb_goroutines, dist_limite := arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6]
 
 	// Connexion du client
 	conn, err := net.Dial("tcp", serv+":"+port)
@@ -63,23 +68,29 @@ func main() {
 	data1 := lecture_csv(bdd1)
 	data2 := lecture_csv(bdd2)
 
-	encoder := gob.NewEncoder(conn)
-	decoder := gob.NewDecoder(conn)
+	// On transforme les paramètres en binaire
+	dataP := []byte(nb_goroutines + " " + dist_limite)
 
 	// Envoi les données au serveur
+	encoder := gob.NewEncoder(conn)
+	// Envoie les paramètres au serveur
+	err = encoder.Encode(dataP)
+	gestion_erreur(err, "Envoi via TCP")
+	// Envoie la première base de données
 	err = encoder.Encode(data1)
 	gestion_erreur(err, "Envoi via TCP")
+	// Envoie la seconde base de données
 	err = encoder.Encode(data2)
 	gestion_erreur(err, "Envoi via TCP")
 
 	fmt.Print("Fichiers envoyés au serveur\n")
 
-	// Réception du fichier final
+	// Réception du fichier final de la part du serveur
+	decoder := gob.NewDecoder(conn)
 	var data []byte
 	nFichier := "final.csv"
 	err = decoder.Decode(&data)
 	gestion_erreur(err, "Décodage")
 	ecriture_csv(data, nFichier)
 	fmt.Printf("Fin de traitement, %q reçu\n", nFichier)
-
 }
--- GO/tcp_serveur.go
+++ GO/tcp_serveur.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func gestion_erreur(err error, message string) {
+	// Arrête le programme si il y a une erreur et affiche un message d'erreur dans le terminal
 	if err != nil {
 		fmt.Printf("Erreur : %s - %v\n", message, err)
 		os.Exit(1) // Arrête le programme avec un code de retour 1 (indiquant une erreur)
@@ -17,11 +19,15 @@ func gestion_erreur(err error, message string) {
 }
 
 func nom_fichier(fichierId string, adrClient string) string {
-	// Date
+	// Définit un fichier en fonction de la date et de l'adresse IP du client
+	// Date du jour
 	now := time.Now()
-	anneeMoisJour := now.Format("2006_01_02")
-	// Nom du fichier
+	// Formate la date et l'heure du jour : jour_mois_an_heure_min_sec
+	anneeMoisJour := now.Format("01_02_2006_15_04_05")
+
 	nomFichier := adrClient + "_" + anneeMoisJour + "_" + fichierId + ".csv"
+
+	// Pour éviter des erreurs, on supprime les caractères spéciaux du nom du fichier
 	c_a_remplacer := []string{"[", "]", ":"}
 	for _, elt := range c_a_remplacer {
 		nomFichier = strings.Replace(nomFichier, elt, "", -1)
@@ -31,50 +37,70 @@ func nom_fichier(fichierId string, adrClient string) string {
 }
 
 func ecriture_csv(data []byte, fichier string) {
+	// Crée un nouveau fichier, si il existe, il est réinitialisé
 	nvFichier, err := os.OpenFile(fichier, os.O_CREATE|os.O_WRONLY|os.O_APPEND|os.O_TRUNC, 0644)
-	defer nvFichier.Close()
+	defer nvFichier.Close() // Ferme le fichier a la fin de la fonction
 	gestion_erreur(err, "Creation du fichier")
+
+	// Ecris les données dans le fichier créé
 	err = os.WriteFile(fichier, data, 0644)
 	gestion_erreur(err, "Ecriture dans fichier")
 }
 
 func main() {
-	gob.Register([]byte{})
+	gob.Register([]byte{}) // Enregistre le type []byte pour le gob
+
 	// On récupère les arguments de la ligne de commande
 	arguments := os.Args
-	if len(arguments) != 2 {
+	if len(arguments) != 2 { // On sort du main si les arguments ne sont pas corrects
 		fmt.Printf("Argument incorrect \n")
 		fmt.Printf("go run programme.go <port>")
 		return
 	}
 	port := arguments[1]
 
-	// Connexion du client
+	// Connexion du serveur
 	conn, err := net.Listen("tcp", ":"+port)
 	gestion_erreur(err, "Serveur à l'écoute")
-	defer conn.Close()
+	defer conn.Close() // Ferme la connexion à la fin du programme
 
-	for {
+	for { // Boucle infinie qui permet d'accepter tous les clients
 		client, err := conn.Accept()
 		gestion_erreur(err, "Connexion du client")
-		go func(client net.Conn) {
-			defer client.Close()
+
+		go func(client net.Conn) { // Permet de gérer plusieurs clients simultanément
+			defer client.Close() // Ferme la connexion à la fin de la goroutine
 			decoder := gob.NewDecoder(client)
 
-			addrClient := client.RemoteAddr().String()
+			addrClient := client.RemoteAddr().String() // Récupère l'adresse ip du client
+
+			// Récupère les paramètres
+			var dataP []byte
+			err = decoder.Decode(&dataP)
+			gestion_erreur(err, "Décodage")
+			param := string(dataP)
+			listeParam := strings.Split(param, " ")
+			nb_goroutines, err := strconv.Atoi(listeParam[0])
+			gestion_erreur(err, "Conversion en int")
+			dist_limite, err := strconv.Atoi(listeParam[1])
+			gestion_erreur(err, "Conversion en int")
+
+			// Récupère le premier fichier CSV
 			nomFichier1 := nom_fichier(addrClient, "1")
 			var data1 []byte
-			err := decoder.Decode(&data1)
+			err = decoder.Decode(&data1)
 			gestion_erreur(err, "Décodage")
 			ecriture_csv(data1, nomFichier1)
 
+			// Récupère le second fichier CSV
 			nomFichier2 := nom_fichier(addrClient, "2")
 			var data2 []byte
 			err = decoder.Decode(&data2)
 			gestion_erreur(err, "Décodage")
 			ecriture_csv(data2, nomFichier2)
 
-			// Appel du main
+			// Appel du main pour le traitement (à implémenter)
+			fmt.Printf("Les paramètres récupérés sont : %v et %v", nb_goroutines, dist_limite)
 			var data = []byte("Test")
 
 			// On renvoie le fichier avec les distances de Levenshtein au client
