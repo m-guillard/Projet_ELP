47686d463b3f014ce4bc9c1aa7ff2fe50154d2da
Marion G. <ma86gui@gmail.com>
2025-01-28 15:52:10 +0100

    Début Parsing

--- ELM/src/parsing.elm
+++ ELM/src/parsing.elm
@@ -1,6 +1,46 @@
 module Parsing exposing (..)
 
-import Parser exposing (suceed, token, int, spaces, lazy, run)
+import Parser exposing (succeed, token, int, spaces, lazy, run)
 
-type perso
-read : String -> perso
+type alias Instructions =
+    { nb : Int
+    , instr : List (List Int)
+    }
+
+type Direction =
+    | Left
+    | Right
+
+type alias Expression =
+    ( String, Int )
+
+intParser : ParserInt
+
+read : String -> Instructions
+read
+
+calcul : Int -> Int -> Dict String Int
+
+parser : Parser.Parser (Instructions)
+parser =
+    lazy(\_ ->
+        spaces
+            |> Parser.andThen(\_ -> token "[")
+            |> Parser.andThen(\_ -> token "Repeat")
+                spaces |> Parser.andThen (\_ ->
+                    int |> Parser.andThen (\value ->
+                        succeed(value)
+                    )
+                )
+            |> Parser.andThen (\_ -> token Direction)
+                spaces |> Parser.andThen (\_ ->
+                        int |> Parser.andThen (\value ->
+                            succeed(value)
+                        )
+                    )
+            |> Parser.andThen (\_ -> parser)
+            |> Parser.andThen(\_ -> token("]"))
+    
+    
+    )
+    
