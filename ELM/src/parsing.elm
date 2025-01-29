fbb35abe147951506c77ba902b029f832b519166
Marion G. <ma86gui@gmail.com>
2025-01-29 21:23:32 +0100

    Résolution erreur merge

--- ELM/src/parsing.elm
+++ ELM/src/parsing.elm
@@ -1,11 +1,33 @@
 module Parsing exposing (..)
 
 import Parser exposing (succeed, token, int, spaces, lazy, run)
+import Array exposing (empty)
 
 type alias Instructions =
     { nb : Int
     , instr : List (List Int)
     }
+
+type InstrIntermediare a
+    = Chaine String
+    | Instruction { nb : Int, instr : InstrIntermediare a}
+
+findParser : Parser.Parser String
+findParser =
+    Parser.oneOf
+        [ token "["
+        , token "]"
+        ]
+
+fonction1 : InstrIntermediare String -> String -> (InstrIntermediare String, String)
+fonction1 instr inputString =
+    case run findParser inputString of
+        Ok _ -> fonction1 (transformationCrochet Instruction)
+        Err _ ->
+            Instruction { nb = 1, instr = instr}
+            
+        
+transformationCrochet : InstrIntermediare -> InstrIntermediare
 
 type Direction =
     | Left
@@ -14,7 +36,26 @@ type Direction =
 type alias Expression =
     ( String, Int )
 
-intParser : ParserInt
+intParser : Parser.Parser Int
+intParser =
+    int
+
+directionParser : Parser.Parser String
+directionParser =
+    Parser.oneOf
+        [ token "Left"
+        , token "Right"
+        , token "Repeat"
+        ]
+
+repeatParser : Parser.Parser String
+repeatParser =
+    Parser.oneOf
+        token "Repeat"
+
+expressionParser : Parser.Parser Expression*
+expressionParser =
+    |= spaces
 
 read : String -> Instructions
 read
