2d5d127df9882da00734f8434779423c9598f3a7
Marion G. <ma86gui@gmail.com>
2025-02-05 08:31:58 +0100

    Ecrire dans un fichier

--- JS/code_marion.js
+++ JS/code_marion.js
@@ -1,20 +1,25 @@
 const fs = require('fs');
 
-function score(prompt, mot, pioche, mots_trouves){
+function score(prompt, mot, pioche, mots_trouves, joueur){
     if (prompt===0) {
         console.log("Tu passes");
+        ecrireDansFichier(`Le joueur ${joueur} passe.`);
     }
     else if (verification(mot, prop) === true){
-        console.log(`Réussite, c'était bien le mot ${mot}`)
+        console.log(`Réussite, c'était bien le mot ${mot}`);
         mots_trouves.push(mot);
+        ecrireDansFichier(`Le joueur ${joueur} a trouvé le mot ${mot}.`);
     }
     else // Erreur
     {
+        ecrireDansFichier(`Le joueur ${joueur} a proposé le mot ${prompt}.`);
+        ecrireDansFichier(`Le mot à trouver était ${mot}.`);
         if(pioche.length>0){
             console.log(`Echec, le mot était ${mot}.J'enlève une carte de la pioche`);
             pioche.shift(); // Supprime le premier mot de la liste
+            ecrireDansFichier(`Une carte de la pioche a été enlevée.`)
         }else{ // Plus de mots dans la pioche, on retire une carte des mots trouvés
-            console.log(`Echec, le mot était ${mot}.J'enlève une carte des mots trouvés`);
+            console.log(`Une carte des mots trouvés a été enlevée.`);
             mots_trouves.shift(); // Supprime le premier mot de la liste
         }
     }
@@ -23,6 +28,7 @@ function score(prompt, mot, pioche, mots_trouves){
 function calcul_score(mots_trouves){
     let score = mots_trouves.length
     console.log(`Fin du jeu ! Vous avez ${score} points.`)
+    ecrireDansFichier(`Fin du jeu ! Les joueurs ont un total de ${score} points.`)
 
     if (score<4){
         console.log("Essayez encore");
@@ -49,7 +55,8 @@ function proposition(indice) {
     return prop;
 }
 
-function ecrireDansFichier(contenu, new_file=true) {
+function ecrireDansFichier(contenu, new_file=false) {
+    contenu += "\n";
     if (new_file===true) {
         fs.writeFile("historique.txt", contenu, (err)=> {
             if (err) {
@@ -58,5 +65,14 @@ function ecrireDansFichier(contenu, new_file=true) {
                 console.log(`Le contenu a été écrit dans ${nomFichier}`);
             }
         })
+    }else {
+        fs.appendFile("historique.txt", contenu + '\n', (err) => {
+            if (err) {
+                console.log('Erreur lors de l\'ajout dans le fichier :', err);
+            } else {
+                console.log(`Le texte a été ajouté dans historique.txt`);
+            }
+        });
     }
+
 }
